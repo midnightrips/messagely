36d579733b5470a9bb70c88032521e8a5707170f
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-10-31 12:47:29 -0400

    Completing exercise

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,10 @@
+const ExpressError = require("../expressError");
+const Router = require("express").Router;
+const router = new Router();
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +17,27 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", ensureLoggedIn, async (req, res, next) => {
+    try {
+        const message = await Message.get(req.params.id);
+
+        // Ensure the message exists
+        if (!message) {
+            throw new ExpressError("Message not found", 404);
+        }
+
+        // Check if the logged-in user is either the sender or receiver
+        const userId = req.user.id;
+        if (message.from_user.id !== userId && message.to_user.id !== userId) {
+            throw new ExpressError("Unauthorized", 401);
+        }
+
+        return res.json({ message });
+    } catch (err) {
+        return next(err);
+    }
+});
+
 
 
 /** POST / - post message.
@@ -18,6 +46,19 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async (req, res, next) => {
+    try {
+        const fromUsername = req.user.username;
+        const { to_username, body } = req.body;
+
+        const message = await Message.create({ from_username: fromUsername, to_username, body });
+
+        return res.status(201).json({ message });
+    } catch (err) {
+        return next(err);
+    }
+});
+
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +68,23 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/:id/read", ensureLoggedIn, async (req, res, next) => {
+    try {
+        const message = await Message.get(req.params.id);
 
+        if (!message) {
+            throw new ExpressError("Message not found", 404);
+        }
+
+        const userId = req.user.id;
+        if (message.to_user.id !== userId) {
+            throw new ExpressError("Unauthorized", 401);
+        }
+
+        const updatedMessage = await Message.markRead(messageId);
+
+        return res.json({ message: updatedMessage });
+    } catch (err) {
+        return next(err);
+    }
+});  
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,23 @@
+const User = require("../models/user")
+const ExpressError = require("../expressError");
+const Router = require("express").Router;
+const router = new Router();
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let users = await User.all();
+        return res.json({ users });
+
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** GET /:username - get detail of users.
@@ -10,7 +25,15 @@
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let user = await User.get(req.params.username);
+        return res.json({ user });
 
+    } catch (err) {
+        return next(err);
+    }
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -21,6 +44,14 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/to", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let messages = await User.messagesTo(req.params.username);
+        return res.json({ messages });
+    } catch (err) {
+        return next(err);
+    }
+});
 
 
 /** GET /:username/from - get messages from user
@@ -32,3 +63,13 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get("/:username/from", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let messages = await User.messagesFrom(req.params.username);
+        return res.json({ messages });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
